b4259cfc2147d3d587c4f406338b98ae4b820d40
TylerLaTruong <101002549+TylerLaTruong@users.noreply.github.com>
2023-11-10 14:40:01 +0700

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
+  tenant_id       = "${var.tenant_id}"
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
   features {}
 }
 terraform {
@@ -16,35 +16,48 @@ terraform {
 
 module "network" {
   source               = "../../modules/network"
-  address_space        = var.address_space
-  location             = var.location
-  virtual_network_name = var.virtual_network_name
-  application_type     = var.application_type
-  resource_type        = "NET"
-  resource_group       = module.resource_group.resource_group_name
-  address_prefix_test  = var.address_prefix_test
+  address_space        = "${var.address_space}"
+  location             = "${var.location}"
+  application_type     = "${var.application_type}"
+  resource_type        = "vnet"
+  resource_group       = "${var.resource_group}"
+  address_prefix_test  = "${var.address_prefix_test}"
 }
 
 module "nsg-test" {
-  source              = "../../modules/networksecuritygroup"
-  location            = var.location
-  application_type    = var.application_type
-  resource_type       = "NSG"
-  resource_group      = module.resource_group.resource_group_name
-  subnet_id           = module.network.subnet_id_test
-  address_prefix_test = var.address_prefix_test
+  source           = "../../modules/networksecuritygroup"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "nsg"
+  resource_group   = "${var.resource_group}"
+  subnet_id        = "${module.network.subnet_id_test}"
+  address_prefix_test = "${var.address_prefix_test}"
 }
 module "appservice" {
   source           = "../../modules/appservice"
-  location         = var.location
-  application_type = var.application_type
-  resource_type    = "AppService"
-  resource_group   = module.resource_group.resource_group_name
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "app-service"
+  resource_group   = "${var.resource_group}"
 }
 module "publicip" {
   source           = "../../modules/publicip"
-  location         = var.location
-  application_type = var.application_type
-  resource_type    = "publicip"
-  resource_group   = module.resource_group.resource_group_name
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "ip"
+  resource_group   = "${var.resource_group}"
+}
+
+module "vm" {
+  source           = "../../modules/vm"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "linux-vm"
+  resource_group   = "${var.resource_group}"
+  subnet_id        = "${module.network.subnet_id_test}"
+  public_ip_address_id = "${module.publicip.public_ip_address_id}"
+  vm_username = "${var.vm_username}"
+  vm_password = "${var.vm_password}"
+  image_name = "${var.image_name}"
+  image_resource_group_name = "${var.resource_group}"
 }
