247d41e4544a2b761b550157f047eea8094be7b5
TylerLaTruong <101002549+TylerLaTruong@users.noreply.github.com>
2023-11-10 14:59:47 +0700

    Update vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,32 +1,42 @@
 resource "azurerm_network_interface" "test" {
-  name                = "NIC-${var.resource_type}-${var.application_type}"
-  location            = var.location
-  resource_group_name = var.resource_group
+  name                = "${var.application_type}-${var.resource_type}-nic"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = var.public_subnet_id
+    subnet_id                     = "${var.subnet_id}"
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = var.public_ip_address_id
+    public_ip_address_id          = "${var.public_ip_address_id}"
   }
 }
 
+data "azurerm_image" "ubuntu" {
+  name                = var.image_name
+  resource_group_name = var.image_resource_group_name
+}
+
 resource "azurerm_linux_virtual_machine" "test" {
-  name                  = "${var.resource_type}-${var.application_type}"
-  location              = var.location
-  resource_group_name   = var.resource_group
-  size                  = "B1s"
-  admin_username        = var.admin_username
+  name                = "${var.application_type}-${var.resource_type}"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
+  size                = "Standard_DS1_v2"
+  admin_username      = "${var.vm_username}"
+  admin_password      = "${var.vm_password}"
+  disable_password_authentication = false
   network_interface_ids = [azurerm_network_interface.test.id]
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCw/NcM4Ec0waiaxTt01dcneRTRB4oOcIdmmFYTOwrcLzw07BlYTagJHeb9ykuOmmaHBK4VaiEUYyOAz9zXD/V93N5wZneo88LP6Ck+JWzRdJEoC1c67QUK3zKnpRo/LUuqLom0QUTHT2HI8oLiXtkFolIHsMEoF4fdGAvO8XJOsat/vQ6RD0coBsYsqtoGAFLw8AglqQB43Ld06rPPlOZyeNNJbiiM40R7sml4l9kgBd4pWofOcyCzDu/D7banlDXcBZPVQtPkfflFT+Fi1zrVa3iYhtlUn6K5UN/LdiTGm8EsLm69SlqYzhuQUf7C28AHQhzaanIFKcbEKGPoldcgddZr979W5FxcTTAFExI0JAdnOy9dUcyrzowmwkVzqI5zeA9wA1CgoYz4H5cZ6NJGFfoJ7fSEFQWd4HvMuD1McKQeHmXQXQkdopZW145FLDqeI492VLEFJy/a6MWS17nWwulgFotMgEGDRtBCl8nmMnvRsDXVCC0mcJbmNKYzkLC7+NwSvWZkIO2hm3yqre+HDnMzBHJendHaTOiX2q4+7DTfeYJG4hQG4pAIDRjlkqnbPAqA4ibyCDUJmNUgmPZsrRbe4WKpD9TrLd1MbSrWdbI6HvKGu14b5XDkWjjWE9eZyy4mEZG5yO6ygO8847Yd+KG4JS9BMIywcCdhO+XO6w== fsoft.fpt.vn\truonglv9@Temp-TruongLV9"
-  }
+  
+  source_image_id = data.azurerm_image.ubuntu.id
+
   os_disk {
-    caching              = "ReadWrite"
+    caching           = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
+
   source_image_reference {
-    id = "/subscriptions/e7a256dc-769a-421c-9f8e-e283ed3cbefa/resourceGroups/Azuredevops/providers/Microsoft.Compute/galleries/UdacityAzureDevOps3Gallery/images/UdacityAzureDevOps3Image/versions/0.0.1"
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
   }
 }
